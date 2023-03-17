302fae41d3954213b359cdd49fb9854cba14aaab
lukavi44 <lukaladisic44@gmail.com>
2023-03-17 16:48:15 +0100

    BookDetails layout modified

--- library-app/src/components/pages/BookDetails/BooksDetails.tsx
+++ library-app/src/components/pages/BookDetails/BooksDetails.tsx
@@ -1,11 +1,16 @@
 import { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
 import { BookBodyDataGet } from '../../../models/bookData.model'
 import { getOneBook, removeBookRequest } from '../../../services/BooksServices'
 import styles from './BookDetails.module.css'
 import placeholder from '../../../assets/placeholderImg/placeholder.jpeg'
 import Modal from '../../Layout/Modal'
 import EditBookForm from '../../Books/BooksList/EditBookForm'
+import DeleteDialog from '../../Layout/DeleteDialog'
+import { convertDateToString } from '../../../helpers/convertDate.helpers'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 
 const BookDetails = () => {
   const [bookDetails, setBookDetails] = useState<BookBodyDataGet>({
@@ -19,13 +24,20 @@ const BookDetails = () => {
     PublishDate: '',
     Quantity: 0,
   })
-  const [isModalOpened, setIsModalOpened] = useState(false)
+  const [isEditModalOpened, setIsEditModalOpened] = useState(false)
+  const [isDeleteModalOpened, setIsDeleteModalOpened] = useState(false)
   const { id } = useParams()
+  const navigate = useNavigate()
 
   useEffect(() => {
     fetchBook()
   }, [])
 
+
+  const notify = (message: string) => {
+     toast(message)
+  };
+  
   const fetchBook = () => {
     if (!id) return
     getOneBook(parseInt(id))
@@ -35,58 +47,91 @@ const BookDetails = () => {
       .catch((error) => console.error(error))
   }
 
+  const onCloseDeleteDialog = () => {
+    setIsDeleteModalOpened(false)
+  }
+
   const deleteBookHandler = () => {
     removeBookRequest(bookDetails.Id)
-      .then((response) => {
-        console.log('successfully deleted', response)
+      .then(() => {
+        notify(`${bookDetails.Title} has been successfully deleted`)
+        navigate('/')
       })
       .catch((error) => console.log(error))
   }
 
   return (
     <div className={styles.container}>
+      <ToastContainer/>
       <div className={styles['container-left']}>
         <h1>{bookDetails?.Title}</h1>
         <ul>
           {bookDetails?.Authors.map((author) => (
             <li key={author.Id}>
-              {author.FirstName} {author.LastName}asdasd
+              {author.FirstName} {author.LastName}Ime Autora
             </li>
           ))}
         </ul>
-        <p>{bookDetails?.Description}</p>
+        <div className={styles['about-book']}>
+          <h3>About {bookDetails.Title}</h3>
+          <p>{bookDetails?.Description}</p>
+        </div>
         <p>{bookDetails?.Isbn}</p>
-        <p>{bookDetails?.PublishDate}</p>
+        {bookDetails.PublishDate && <p>Publish Date:</p>}
+        {bookDetails.PublishDate ? <p>{convertDateToString(bookDetails.PublishDate)}</p> : ''}
       </div>
       <div className={styles['container-right']}>
         <img
           src={bookDetails?.Cover ? `data:image/png;base64, ${bookDetails?.Cover}` : placeholder}
           alt='Book Cover'
         />
+        <div className={styles['book-quantity']}>
+          <p>Quantity </p>
+          <p className={styles['book-quantity-number']}>{bookDetails.Quantity}</p>
+        </div>
         <div
-          className={bookDetails.Quantity > 0 ? `${styles.quantity}` : `${styles['quantity-zero']}`}
+          className={
+            bookDetails.Available > 0 ? `${styles.available}` : `${styles['available-zero']}`
+          }
         >
           <p>Available</p>
-          <p>{bookDetails.Quantity}</p>
+          <p>{bookDetails.Available}</p>
         </div>
       </div>
       <div className={styles['actions-btn-holder']}>
         <button
+          style={{ background: '#d9b99b' }}
           className={styles['action-btn']}
           id={styles.edit}
-          onClick={() => setIsModalOpened(true)}
+          onClick={() => {
+            setIsEditModalOpened(true)
+          }}
         >
           Edit
         </button>
-        {isModalOpened && (
-          <Modal onClose={() => setIsModalOpened(false)}>
+        {isEditModalOpened && (
+          <Modal onClose={() => setIsEditModalOpened(false)}>
             <EditBookForm book={bookDetails} />
           </Modal>
         )}
-        <button className={styles['action-btn']} id={styles.delete} onClick={deleteBookHandler}>
+        <button
+          style={{ background: '#eed9c4' }}
+          className={styles['action-btn']}
+          id={styles.delete}
+          onClick={() => setIsDeleteModalOpened(true)}
+        >
           Delete
         </button>
-        <button className={styles['action-btn']} id={styles.rent}>
+        {isDeleteModalOpened && (
+          <Modal onClose={() => setIsDeleteModalOpened(false)}>
+            <DeleteDialog
+              closeDeleteDialog={onCloseDeleteDialog}
+              deleteBook={deleteBookHandler}
+              book={bookDetails}
+            />
+          </Modal>
+        )}
+        <button style={{ background: '#fff0db' }} className={styles['action-btn']} id={styles.rent}>
           Rent
         </button>
       </div>
